bfd16f25b33fe6401e3386a5aa59b553a504b7c0
TheRealMesteven <mestevenyoutubing@gmail.com>
2025-05-07 20:49:05 +0100

    Add project files.

---
+++ BotCount/Mod.cs
@@ -0,0 +1,14 @@
+﻿using PulsarModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCount
+{
+    internal class Mod : PulsarMod
+    {
+        public override string HarmonyIdentifier() => "Mest.BotCount";
+    }
+}
